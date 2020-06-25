a0cbb7d2bcc2d79ba7d9ec02d8cbf320a1a98051
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-06-25 14:06:57 +0530

    Adding files

--- game.js
+++ game.js
@@ -3,3 +3,6 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
 ctx.fillRect(0, 0, 150, 75);
+ctx.moveTo(200, 100);
+ctx.lineTo(400, 4000);
+ctx.stroke();
