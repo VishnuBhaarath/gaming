2b592c0ba92f23197be8ad6f8448077ece968180
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-06-26 22:08:23 +0530

    Adding controllers

--- firstgame.js
+++ firstgame.js
@@ -23,16 +23,36 @@ function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
     this.x = x;
-    this.y = y;    
+    this.y = y;   
+    this.speedX = 0;
+    this.speedY = 0; 
     this.update = function(){
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+     this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }    
+}
+function moveup() {
+    myGamePiece.speedY -= 1; 
 }
 
+function movedown() {
+    myGamePiece.speedY += 1; 
+}
+
+function moveleft() {
+    myGamePiece.speedX -= 1; 
+}
+
+function moveright() {
+    myGamePiece.speedX += 1; 
+}
 function updateGameArea() {
     myGameArea.clear();
-    myGamePiece.x += 1;
+    myGamePiece.newPos(); 
     myGamePiece.update();
 }
