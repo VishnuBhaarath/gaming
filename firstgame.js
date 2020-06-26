708595e98103f3a8fd9f05eee86882b3a49fdec8
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-06-26 12:35:24 +0530

    Adding controllers

--- firstgame.js
+++ firstgame.js
@@ -3,17 +3,43 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x=120;
 var y=10;
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(10, x, 30, 30);
-setInterval(drawsquare, 20);
-function drawsquare(){
+var speedY=0;
+var speedX=0;
+drawcomponent()
+function drawcomponent(){
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(y, x, 30, 30);
+ 
+}
+function updatesquare(){
 
       ctx.fillStyle = "#FF0000";
-      ctx.clearRect(10, x, 30, 30);
-      x=x+1;
-      ctx.fillRect(10, x, 30, 30);
- 
-     
-     
       
+     
+      ctx.fillRect(y, x, 30, 30);
+ }
+ function moveup() {
+   
+     ctx.clearRect(y, x, 30, 30);
+      x=x-1;
+     updatesquare()
+}
+
+function movedown() {
+	ctx.clearRect(y, x, 30, 30);
+    x=x+1;
+    updatesquare()
+}
+
+function moveleft() {
+	ctx.clearRect(y, x, 30, 30);
+    y=y+1;
+    updatesquare()
+ 
+}
+
+function moveright() {
+	ctx.clearRect(y, x, 30, 30);
+    y=y-1;
+    updatesquare()
 }
