307ff4d86db550bb219038dd41f7a7faa0132806
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-06-26 10:06:48 +0530

    moving object

--- firstgame.js
+++ firstgame.js
@@ -3,16 +3,16 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x=120;
 var y=10;
-
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(10, x, 30, 30);
 setInterval(drawsquare, 20);
-
 function drawsquare(){
 
       ctx.fillStyle = "#FF0000";
-      ctx.fillRect(10, x, 30, 30);
-       ctx.clearRect(10, x, canvas.width, canvas.height);
+      ctx.clearRect(10, x, 30, 30);
       x=x+1;
-      y=y+1;
+      ctx.fillRect(10, x, 30, 30);
+ 
      
      
       
