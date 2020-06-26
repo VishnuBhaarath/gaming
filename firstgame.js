210065a48cbd48442b2387d43983b868df7d5c6d
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-06-26 23:31:04 +0530

    Random obstacles

--- firstgame.js
+++ firstgame.js
@@ -22,7 +22,7 @@ var myGameArea = {
     },
     stop : function() {
         clearInterval(this.interval);
-    }    
+    }
 }
 
 function component(width, height, color, x, y) {
@@ -59,7 +59,7 @@ function component(width, height, color, x, y) {
 }
 
 function updateGameArea() {
-    var x, y;
+    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
     for (i = 0; i < myObstacles.length; i += 1) {
         if (myGamePiece.crashWith(myObstacles[i])) {
             myGameArea.stop();
@@ -70,8 +70,14 @@ function updateGameArea() {
     myGameArea.frameNo += 1;
     if (myGameArea.frameNo == 1 || everyinterval(150)) {
         x = myGameArea.canvas.width;
-        y = myGameArea.canvas.height - 200;
-        myObstacles.push(new component(10, 200, "green", x, y));
+        minHeight = 20;
+        maxHeight = 200;
+        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+        minGap = 50;
+        maxGap = 200;
+        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+        myObstacles.push(new component(10, height, "green", x, 0));
+        myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
     }
     for (i = 0; i < myObstacles.length; i += 1) {
         myObstacles[i].x += -1;
