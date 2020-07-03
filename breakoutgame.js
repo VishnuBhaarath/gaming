a84477ccff51123eb70573dfe2bb5ef63e86c4fb
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-07-04 00:00:32 +0530

    functions

--- breakoutgame.js
+++ breakoutgame.js
@@ -1,14 +1,21 @@
 var canvas = document.getElementById("mycanvas");
 var ctx=canvas.getContext("2d");
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.fillStyle="red";
-ctx.fill();
-ctx.closePath();
 function startGame() {
 myGamePiece = new component(55, 15, "red", 210, 305);
+mycircularball=new Ball(120,80,15,"red",0,2*Math.PI);
 }
-
+function Ball(x,y,radius,color,startangle,endangle){
+	this.x=x;
+    this.y=y;
+    this.radius=radius;
+    this.startangle=startangle;
+    this.endangle=endangle;
+    ctx.beginPath();
+    ctx.arc(this.x,this.y,this.radius,this.startangle,this.endangle);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.closePath();
+}
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
