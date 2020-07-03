b79057cbe199ef103d6392dbd18b45c576ab6b6d
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-07-03 23:48:50 +0530

    Setting components

--- breakoutgame.js
+++ breakoutgame.js
@@ -1,4 +1,19 @@
 var canvas = document.getElementById("mycanvas");
 var ctx=canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(210, 305, 55, 15);
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.fillStyle="red";
+ctx.fill();
+ctx.closePath();
+function startGame() {
+myGamePiece = new component(55, 15, "red", 210, 305);
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    ctx.fillStyle = color;
+    this.x=x;
+    this.y=y;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+}
